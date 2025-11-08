39da83b2d151c590dc5ad423de73d5b71fdce99a
Shannon Oram <shannon.oram@gmail.com>
2025-11-08 23:48:13 +1100

    Adding extra test cases

--- src/utils/getRelatedFileCandidates.test.ts
+++ src/utils/getRelatedFileCandidates.test.ts
@@ -9,4 +9,25 @@ describe("getSourceCandidates", () => {
 		const candidates = getSourceCandidates(properties);
 		expect(candidates).toEqual(["path/components/FancyButton.tsx"]);
 	});
+
+	test("should return candidates for /tests/FancyButton.test.tsx", async () => {
+		const properties = inferFileProperties(
+			"path/components/tests/FancyButton.test.tsx",
+		);
+		const candidates = getSourceCandidates(properties);
+		expect(candidates).toEqual(["path/components/FancyButton.tsx"]);
+	});
+
+	test("should return candidates for /styles/FancyButton.module.scss", async () => {
+		const properties = inferFileProperties(
+			"path/components/styles/FancyButton.module.scss",
+		);
+		const candidates = getSourceCandidates(properties);
+		expect(candidates).toEqual([
+			"path/components/FancyButton.tsx",
+			"path/components/FancyButton.ts",
+			"path/components/FancyButton.jsx",
+			"path/components/FancyButton.js",
+		]);
+	});
 });
--- src/utils/getRelatedFileCandidates.ts
+++ src/utils/getRelatedFileCandidates.ts
@@ -3,6 +3,44 @@ import type { FileProperties } from "./inferFileProperties";
 import { trimLastDirIfMatch } from "./trimLastDir";
 
 export const getSourceCandidates = (properties: FileProperties): string[] => {
-	const dirname = trimLastDirIfMatch(properties.dirname, ["stories"]);
-	return [[dirname, `${properties.basename}${properties.ext}`].join(path.sep)];
+	const dirname = trimLastDirIfMatch(properties.dirname, [
+		"stories",
+		"tests",
+		"styles",
+	]);
+
+	switch (properties.type) {
+		case "storybook": {
+			return [
+				[dirname, `${properties.basename}${properties.ext}`].join(path.sep),
+			];
+		}
+		case "style": {
+			return [
+				[dirname, `${properties.basename}.tsx`].join(path.sep),
+				[dirname, `${properties.basename}.ts`].join(path.sep),
+				[dirname, `${properties.basename}.jsx`].join(path.sep),
+				[dirname, `${properties.basename}.js`].join(path.sep),
+			];
+		}
+		case "source": {
+			return [
+				[dirname, `${properties.basename}${properties.ext}`].join(path.sep),
+			];
+		}
+		case "test": {
+			return [
+				[dirname, `${properties.basename}${properties.ext}`].join(path.sep),
+			];
+		}
+		case "unknown": {
+			return [
+				[dirname, `${properties.basename}${properties.ext}`].join(path.sep),
+			];
+		}
+		default: {
+			const _exhaustiveCheck: never = properties.type;
+			return _exhaustiveCheck;
+		}
+	}
 };
