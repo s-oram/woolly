fb8ee31140c8a9d38cf41727a94cea9c57c9c78b
Shannon Oram <shannon.oram@gmail.com>
2025-11-08 18:10:46 +1100

    Rename extension

--- src/extension.ts
+++ src/extension.ts
@@ -1,16 +1,28 @@
-import * as vscode from 'vscode';
-import { openSource, openStories, openStyles, openTests } from './commands';
+import * as vscode from "vscode";
+import { openSource, openStories, openStyles, openTests } from "./commands";
 
 export function activate(context: vscode.ExtensionContext) {
-	const outputChannel = vscode.window.createOutputChannel('Goto Code');
-	outputChannel.append('initialized');
+	const outputChannel = vscode.window.createOutputChannel("Go To Code");
+	outputChannel.append("initialized");
 
 	outputChannel.show(false);
 
-	context.subscriptions.push(vscode.commands.registerCommand('goto-code.openSource', openSource(outputChannel)));
-	context.subscriptions.push(vscode.commands.registerCommand('goto-code.openStories', openStories));
-	context.subscriptions.push(vscode.commands.registerCommand('goto-code.openStyles', openStyles));
-	context.subscriptions.push(vscode.commands.registerCommand('goto-code.openTests', openTests));
+	context.subscriptions.push(
+		vscode.commands.registerCommand(
+			"go-to-code.openSource",
+			openSource(outputChannel),
+		),
+	);
+
+	context.subscriptions.push(
+		vscode.commands.registerCommand("go-to-code.openStories", openStories),
+	);
+	context.subscriptions.push(
+		vscode.commands.registerCommand("go-to-code.openStyles", openStyles),
+	);
+	context.subscriptions.push(
+		vscode.commands.registerCommand("go-to-code.openTests", openTests),
+	);
 
 	context.subscriptions.push(outputChannel);
 }
